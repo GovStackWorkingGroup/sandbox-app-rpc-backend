dfca310186c9cb14ef7df6f93bd3d2d5d6822a00
vladislav.todorov <vladislav.todorov@athlonsofia.com>
2023-10-18 12:10:28 +0300

    Init Springboot Gradle app + updated gitignore

---
+++ src/main/java/global/govstack/rpcbackend/RpcbackendApplication.java
@@ -0,0 +1,12 @@
+package global.govstack.rpcbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpcbackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RpcbackendApplication.class, args);
+  }
+}
---
+++ src/test/java/global/govstack/rpcbackend/RpcbackendApplicationTests.java
@@ -0,0 +1,11 @@
+package global.govstack.rpcbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpcbackendApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
