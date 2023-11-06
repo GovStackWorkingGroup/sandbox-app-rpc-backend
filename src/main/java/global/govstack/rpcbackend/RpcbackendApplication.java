56c2753f9c39ba0897760c91a47c590396e8697c
vladislav.todorov <vladislav.todorov@athlonsofia.com>
2023-11-06 15:30:26 +0200

    SND-612: Migration to add users & User endpoint

--- src/main/java/global/govstack/rpcbackend/RpcbackendApplication.java
+++ src/main/java/global/govstack/rpcbackend/RpcbackendApplication.java
@@ -1,8 +1,6 @@
 package global.govstack.rpcbackend;
 
-import global.govstack.rpcbackend.model.Role;
-import global.govstack.rpcbackend.repository.RoleRepository;
-import org.springframework.boot.CommandLineRunner;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -15,12 +13,7 @@ public class RpcbackendApplication {
   }
 
   @Bean
-  public CommandLineRunner demo(RoleRepository roleRepo) {
-    return (args) -> {
-      Role role = new Role();
-      role.setId(1);
-      role.setName("ROLE_USER");
-      roleRepo.save(role);
-    };
+  public ModelMapper modelMapper() {
+    return new ModelMapper();
   }
 }
